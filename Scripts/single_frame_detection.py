7705d9d0896dc07192cfe9d683fd491d25b2088c
Pradyumn Pathak <32090314+Stark16@users.noreply.github.com>
2021-01-05 11:01:41 +0530

    Update single_frame_detection.py
    Python script that detects distinct faces in each frame in real-time.

--- Scripts/single_frame_detection.py
+++ Scripts/single_frame_detection.py
@@ -1,7 +1,7 @@
 import cv2
 import face_recognition
 
-# Video explaining basic multithreading: https://www.youtube.com/watch?v=vbtxtvuCFRM
+
 frames = []
 video_path = './../Videos/Input_video.mp4'
 cap = cv2.VideoCapture(video_path)
